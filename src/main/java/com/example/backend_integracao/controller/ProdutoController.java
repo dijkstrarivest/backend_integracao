173e7be7f5e3e67072da470162b52eb6900e2bd5
João Paulo F. C. César <joaocesar@souunilavras.com>
2023-11-22 16:49:07 -0300

    correções

--- src/main/java/com/example/backend_integracao/controller/ProdutoController.java
+++ src/main/java/com/example/backend_integracao/controller/ProdutoController.java
@@ -18,47 +18,63 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    // Endpoint para obter todos os produtos com suporte a paginação
     @GetMapping
-    public Page<Produto> getAllProdutos(@RequestParam(defaultValue = "0") int page,
-                                       @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return produtoRepository.findAll(pageable);
+    public ResponseEntity<Page<Produto>> getAllProdutos(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        try {
+            Pageable pageable = PageRequest.of(page, size);
+            Page<Produto> produtos = produtoRepository.findAll(pageable);
+            return ResponseEntity.ok(produtos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    // Endpoint para obter um produto por ID
     @GetMapping("/{id}")
     public ResponseEntity<Produto> getProdutoById(@PathVariable Long id) {
-        Optional<Produto> produto = produtoRepository.findById(id);
-        return produto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        try {
+            Optional<Produto> produto = produtoRepository.findById(id);
+            return produto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    // Endpoint para adicionar um novo produto
     @PostMapping
     public ResponseEntity<Produto> createProduto(@RequestBody Produto produto) {
-        Produto savedProduto = produtoRepository.save(produto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduto);
+        try {
+            Produto savedProduto = produtoRepository.save(produto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedProduto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    // Endpoint para atualizar um produto existente
     @PutMapping("/{id}")
     public ResponseEntity<Produto> updateProduto(@PathVariable Long id, @RequestBody Produto produto) {
-        if (!produtoRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
+        try {
+            if (!produtoRepository.existsById(id)) {
+                return ResponseEntity.notFound().build();
+            }
+            produto.setId(id);
+            Produto updatedProduto = produtoRepository.save(produto);
+            return ResponseEntity.ok(updatedProduto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        produto.setId(id);
-        Produto updatedProduto = produtoRepository.save(produto);
-        return ResponseEntity.ok(updatedProduto);
     }
 
-    // Endpoint para excluir um produto por ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduto(@PathVariable Long id) {
-        if (!produtoRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
+        try {
+            if (!produtoRepository.existsById(id)) {
+                return ResponseEntity.notFound().build();
+            }
+            produtoRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        produtoRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 }
-
