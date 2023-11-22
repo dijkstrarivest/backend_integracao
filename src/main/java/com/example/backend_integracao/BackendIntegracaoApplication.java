f0ce8458d31bf95a064befcdd75eaaa007389b81
João Paulo F. C. César <joaocesar@souunilavras.com>
2023-11-22 17:43:51 -0300

    final

--- src/main/java/com/example/backend_integracao/BackendIntegracaoApplication.java
+++ src/main/java/com/example/backend_integracao/BackendIntegracaoApplication.java
@@ -3,8 +3,10 @@ package com.example.backend_integracao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories(basePackages = "com.example.backend_integracao.repository")
 public class BackendIntegracaoApplication {
 
 	public static void main(String[] args) {
