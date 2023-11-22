cd262ca94a59442fc9419cc96e5f944cd77a1f52
João Paulo F. C. César <joaocesar@souunilavras.com>
2023-11-22 17:37:14 -0300

    final

--- src/main/java/com/example/backend_integracao/repository/ProdutoRepository.java
+++ src/main/java/com/example/backend_integracao/repository/ProdutoRepository.java
@@ -2,7 +2,9 @@ package com.example.backend_integracao.repository;
 
 import com.example.backend_integracao.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     // Interface do Spring Data JPA para realizar operações CRUD no banco de dados
 }
